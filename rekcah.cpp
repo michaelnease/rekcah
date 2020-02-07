b35418a062ac84cfb4dadda52419f26621c154cf
Michael Nease <michaelnease@michaelnease.com>
2020-02-06 20:19:35 -0800

    init commit

---
+++ rekcah.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+
+  std::cout << "You have 3 seconds to hack the server ... 😮";
+  std::cout << std::endl;
+  std::cout << "Enter to correct code to continue ...";
+  std::cout << std::endl;
+
+  int a = 4;
+  int b = 5;
+  int c = 6;
+
+  std::cout << a + b + c;
+  std::cout << std::endl;
+
+  return 0;
+}
